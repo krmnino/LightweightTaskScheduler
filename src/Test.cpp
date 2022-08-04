d6b9bd601339db7ae6460ee0ea9e86e397f7de05
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-08-04 00:47:06 -0400

    implemented validate_hms() and time_now_add_dhms()

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,6 +3,44 @@
 
 #define TIMEZONE -4
 
+bool validate_hms(std::string hms){
+    if(hms.length() != 8){
+        return false;
+    }
+    unsigned int num;
+    num = hms.at(0) & 0x0F;
+    if(num < 0 | num > 2){
+        return false;
+    }
+    num = hms.at(1) & 0x0F;
+    if(num < 0 | num > 9){
+        return false;
+    }
+    if(hms.at(2) != ':'){
+        return false;
+    }
+    num = hms.at(3) & 0x0F;
+    if(num < 0 | num > 5){
+        return false;
+    }
+    num = hms.at(4) & 0x0F;
+    if(num < 0 | num > 9){
+        return false;
+    }
+    if(hms.at(5) != ':'){
+        return false;
+    }
+    num = hms.at(6) & 0x0F;
+    if(num < 0 | num > 5){
+        return false;
+    }
+    num = hms.at(7) & 0x0F;
+    if(num < 0 | num > 9){
+        return false;
+    }
+    return true;
+}
+
 time_t init_day(){
     std::time_t time_now;
     std::tm* struct_time_now;
@@ -20,17 +58,16 @@ time_t init_day(){
     // Add converted hours and minutes to total seconds
     sec += hrs_2_sec + min_2_sec;
 
-    std::cout << ctime(&time_now) << std::endl;
-
     // Subtract time
     time_now -= sec;
-
-    std::cout << ctime(&time_now) << std::endl;
 
     return time_now;
 }
 
 time_t time_now_add_hms(std::string hms){
+    if(!validate_hms(hms)){
+        return 0;
+    }
     std::tm time_input_hms;
     std::istringstream hms_ss(hms.c_str());
     hms_ss >> std::get_time(&time_input_hms, "%H:%M:%S");
@@ -49,42 +86,51 @@ time_t time_now_add_hms(std::string hms){
 
     time_now += sec;
 
-    std::cout << ctime(&time_now) << std::endl;
+    return time_now;
+}
+
+time_t time_now_add_dhms(int d, std::string hms){
+    if(!validate_hms(hms) | d < 0){
+        return 0;
+    }
+    std::tm time_input_hms;
+    std::istringstream hms_ss(hms.c_str());
+    hms_ss >> std::get_time(&time_input_hms, "%H:%M:%S");
+
+    std::time_t time_hms = mktime(&time_input_hms);
+
+    time_t days_2_sec = d * 24 * 60 * 60;
+
+    // Get hours, minutes, and seconds. Convert to seconds 
+    int hrs_2_sec = time_input_hms.tm_hour * 60 * 60;
+    int min_2_sec = time_input_hms.tm_min * 60;
+    int sec = time_input_hms.tm_sec;
+
+    // Add converted days, hours, and minutes to total seconds
+    sec += days_2_sec + hrs_2_sec + min_2_sec;
+
+    std::time_t time_now = init_day();
+
+    time_now += sec;
 
     return time_now;
 }
 
 int main(){
+    time_t ret;
     init_day();
-    time_now_add_hms("12:34:90");
-    /*
-    // get current time
-    std::time_t tm_now;
-    std::time(&tm_now);
 
-    struct std::tm tm;
-    std::istringstream ss("12:00:00");
-    ss >> std::get_time(&tm, "%H:%M:%S"); // or just %T in this case
+    ret = time_now_add_hms("09:34:00");
+    std::cout << ctime(&ret) << std::endl;
 
-    int sec_hours = tm.tm_hour * 60 * 60;
-    int sec_minutes = tm.tm_min * 60;
-    int seconds = tm.tm_sec;
-    int total = sec_hours + sec_minutes + seconds;
+    ret = time_now_add_dhms(1, "01:34:00");
+    std::cout << ctime(&ret) << std::endl;
 
-    //std::cout << total << std::endl;
+    ret = time_now_add_dhms(9000, "01:34:00");
+    std::cout << ctime(&ret) << std::endl;
 
-    std::time_t time = mktime(&tm);
-    std::time_t new_time = tm_now - total;
+    bool v = validate_hms("01:34:00");
+    std::cout << v << std::endl;
 
-    std::gmtime(&tm_now);
-    std::gmtime(&new_time);
-
-    //std::cout << ctime(&time);
-    //std::cout << ctime(&tm_now);
-    std::cout << ctime(&new_time);
-
-
-    std::cout << tm.tm_hour << std::endl;
-    */
     return 0;
 }
