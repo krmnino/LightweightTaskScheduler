ab2a6a744f1224a433585f83d2e1ccd979cdc6c4
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2022-10-23 14:22:05 -0400

    updated main() and removed Task.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -49,14 +49,14 @@
 
 int main(){
     
-    //cl::Config* main_config = new cl::Config("config/main_config.cl");
-    //std::string top_level_dir = main_config->get_value("TopLevel")->get_data<std::string>();
-    //std::string tasks_dir = main_config->get_value("TasksDir")->get_data<std::string>();
-    //std::string scripts_dir = main_config->get_value("ScriptsDir")->get_data<std::string>();
-    //delete main_config;
+    cl::Config* main_config = new cl::Config("config/main_config.cl");
+    std::string top_level_dir = main_config->get_value("TopLevel")->get_data<std::string>();
+    std::string tasks_dir = main_config->get_value("TasksDir")->get_data<std::string>();
+    std::string scripts_dir = main_config->get_value("ScriptsDir")->get_data<std::string>();
+    delete main_config;
 
-    //ts::Scheduler* s = new ts::Scheduler(top_level_dir, tasks_dir, scripts_dir);
-    //delete s;
+    ts::Scheduler* s = new ts::Scheduler(top_level_dir, tasks_dir, scripts_dir);
+    delete s;
 
     //ts::Task* t = new ts::Task("my task", "sh hello.sh");
     //t->launch();
